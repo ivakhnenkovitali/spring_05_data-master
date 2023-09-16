6db0f702ea6623ee4d83fdd00e5bbc2390a804c6
Asus <vaxa2320@gmail.com>
2023-09-16 17:04:55 +0300

    n_24

--- spring_05_data-master/src/main/java/by/IT/_02_jpa/repositories/AirplaneRepository.java
+++ spring_05_data-master/src/main/java/by/IT/_02_jpa/repositories/AirplaneRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
 @Repository
 public interface AirplaneRepository extends CrudRepository<Airplane, Integer> {
     Airplane findByModel(String model);

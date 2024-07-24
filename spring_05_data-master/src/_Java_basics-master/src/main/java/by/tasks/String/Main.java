3e080858b5dda0e752bef540c3bd7617744f48ad
Asus <vaxa2320@gmail.com>
2024-07-24 19:13:25 +0300

    n_27

--- spring_05_data-master/src/_Java_basics-master/src/main/java/by/tasks/String/Main.java
+++ spring_05_data-master/src/_Java_basics-master/src/main/java/by/tasks/String/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 public class Main {
     // Дан текст на русском
     // Длинна - не более NL символов

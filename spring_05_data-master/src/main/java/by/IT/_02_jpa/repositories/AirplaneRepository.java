52987097d1e153e69569dfb4ed2870cfb17df970
Asus <vaxa2320@gmail.com>
2024-09-04 16:04:15 +0300

    n_26

--- spring_05_data-master/src/main/java/by/IT/_02_jpa/repositories/AirplaneRepository.java
+++ spring_05_data-master/src/main/java/by/IT/_02_jpa/repositories/AirplaneRepository.java
@@ -2,7 +2,6 @@ package by.IT._02_jpa.repositories;
 import by.itclass._02_jpa.entities.Airplane;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 @Repository
 public interface AirplaneRepository extends CrudRepository<Airplane, Integer> {
